c8d221d42aa9c5d7752d0163e4589889a91d3d66
Ann Kornilova <Kirbri@yandex.ru>
2024-09-11 17:02:16 +0300

    Create class selenideGithubHaveCodeForJUnit5

---
+++ src/test/java/tests/selenideGithubHaveCodeForJUnit5.java
@@ -0,0 +1,41 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class selenideGithubHaveCodeForJUnit5 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void selenideCodeTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#js-repo-pjax-container").shouldHave(text("SoftAssertions"));
+        $("#wiki-body").shouldBe(text("@Listeners({ SoftAsserts.class})\n" +
+                "public class Tests {\n" +
+                "  @Test\n" +
+                "  public void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
